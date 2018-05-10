95b26a303fbc2c6d6798bf5ae849122e9e52670f
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2018-05-10 17:15:31 +0300

    Update and rename rb_tree.hpp to graph.hpp

---
+++ include/graph.hpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+class Graph
+{
+private:
+    vector<vector<unsigned> > graph;
+    vector<unsigned> result;
+    unsigned N;
+
+public:
+    Graph()
+    {
+        N = 0;
+    }
+
+    ~Graph()
+    {
+        N = 0;
+    }
+
+    Graph(vector<vector<unsigned> > input)
+    {
+        for (unsigned int i = 0; i < input.size(); ++i)
+        {
+            for (const auto& j : input[i])
+            {
+                if (j >= input.size())
+                    throw length_error("fail");
+            }
+        }
+        graph = input;
+        N = graph.size();
+    }
+
+    void read(istringstream& stream)
+    {
+        graph.clear();
+        string stroka;
+        while (getline(stream, stroka))
+        {
+            istringstream stream(stroka);
+            unsigned n;
+            char op;
+            if (stream >> n && stream >> op && op == ':')
+            {
+                N = n + 1;
+                int value;
+                vector<unsigned> time;
+                while (stream >> value)
+                {
+                    time.push_back(value);
+                }
+                graph.push_back(time);
+            }
+        }
+    }
+
+    Graph(Graph& other)
+    {
+        N = other.N;
+        graph = other.graph;
+        if (!(other.result.empty()))
+            result = other.result;
+    }
+
+    Graph operator=(Graph& other)
+    {
+        if (this != &other)
+        {
+            N = other.N;
+            graph = other.graph;
+            if (!other.result.empty())
+                result = other.result;
+        }
+        return (*this);
+    }
+
+    void dfs(unsigned index)
+    {
+        vector<bool> run_;
+        result.clear();
+        for (unsigned i = 0; i < N; ++i)
+        {
+            run_.push_back(false);
+        }
+        dfs_help(index, &run_);
+    }
+
+    void dfs_help(unsigned index, vector<bool>* run_)
+    {
+        (*run_)[index] = true;
+        result.push_back(index);
+        for (const auto& i : graph[index])
+        {
+            if (!(*run_)[i])
+                dfs_help(i, run_);
+        }
+    }
+
+    void print_result(ostream& stream)
+    {
+        for (unsigned i : result)
+            stream << "->" << i;
+    }
+
+    void print_graph(ostream& stream)
+    {
+        for (unsigned i = 0; i < N; i++)
+        {
+            stream << i << ":";
+            for (const auto j : graph[i])
+            {
+                stream << j << " ";
+            }
+            stream << endl;
+        }
+    }
+};
